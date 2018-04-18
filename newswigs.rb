8216d5d55b38d1fe270722e32a335a4a77818dab
Libby Miller <libby@nicecupoftea.org>
2018-04-18 16:14:55 +0100

    reformatted for better Moo printing

--- newswigs.rb
+++ newswigs.rb
@@ -1,8 +1,10 @@
 require 'squib'
 
-Squib::Deck.new(width: 1125, height: 825, cards: 8, layout: 'layout.yml') do
+Squib::Deck.new(width: 1250, height: 900, cards: 8, layout: 'layout.yml', dpi: 300) do
   background color: '#ffffff'
   deck = xlsx file: 'newswigs.xlsx'
+
+#  safe_zone
 
   text str: deck['Title'], layout: :Title
 
@@ -14,6 +16,8 @@ Squib::Deck.new(width: 1125, height: 825, cards: 8, layout: 'layout.yml') do
 
   svg file: deck['Right_i'], layout: :RightIcon
   text str: deck['Right'], layout: :RightText
+
+  svg file: 'svgs/arrow.svg', layout: :CentreArrow
 
   save_png prefix: 'newswigs_'
 
